1f6af24491f080f43988626f8535afb0f2655693
佐々木　謙 <nkekisasa222@gmail.com>
2021-09-24 15:58:58 +0900

    feat: 簡易的な見た目を作成

--- ExperienceTodo/ViewController.swift
+++ ExperienceTodo/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setTableView()
+        setNavigation()
     }
+    
+    private func setNavigation() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor.systemTeal
+        appearance.titleTextAttributes =  [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.systemFont(ofSize: 18, weight: .medium)
+        ]
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+    
+    private func setTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBAction func tapAddButton(_ sender: Any) {
+        print("追加")
+    }
+    
 
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let todoText = cell.viewWithTag(1) as? UILabel
+        todoText?.text = "散歩行く"
+        
+        return cell
+    }
 }
 
